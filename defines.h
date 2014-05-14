126c9fdcc190bf9b00ef4ec3c669ddb1a0820c00
Bret Truchan <clone45@gmail.com>
2014-05-14 08:27:06 -0700

    Added more wavetables, making the total 32.

--- defines.h
+++ defines.h
@@ -5,7 +5,7 @@
 
 #define NUMBER_OF_EQUATIONS 53              // # of equations in Equations.cpp
 #define NUMBER_OF_WAVETABLE_EQUATIONS 16    // # of equations in EquationsWavetable.cpp
-#define NUMBER_OF_WAVETABLES 23
+#define NUMBER_OF_WAVETABLES 32
 #define WAVE_SAMPLES 512
 
 #define MAX_CV 4095
