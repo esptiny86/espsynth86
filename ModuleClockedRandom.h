b74a3acede347759a6a9a29d19a0c5b9609d6e02
Bret Truchan <clone45@gmail.com>
2014-05-31 16:10:09 -0700

    Added a bit of documentation for ModuleClockedRandom

--- ModuleClockedRandom.h
+++ ModuleClockedRandom.h
@@ -10,7 +10,24 @@
 */
 // =============================================================================
 //
- 
+// ModuleClockedRandom is a clocked random number generator.  The output ranges
+// from 0 to 4095.
+//
+// Example usage:
+//
+//    ModuleWavetableOsc *osc = new ModuleWavetableOsc();
+//    ModuleClockedRandom *clocked_random = new ModuleClockedRandom();
+//    ModuleExtClock *ext_clock = new ModuleExtClock(120, EIGHTH_NOTE_CLOCK_DIVISION);
+//
+//    ext_clock->clock_input = inputs->gate;
+//
+//    clocked_random->clock_input = ext_clock;
+//
+//    osc->frequency_input = clocked_random;
+//
+//    this->last_module = osc;
+//
+
 #ifndef ModuleClockedRandom_h
 #define ModuleClockedRandom_h
 
