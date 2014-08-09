19c55779ac24f02833c6ba453354b210e0386bc6
Bret Truchan <clone45@gmail.com>
2014-08-08 17:33:25 -0700

    Fixed bug where analog inputs would influence each other.

--- ModuleAnalogInput.cpp
+++ ModuleAnalogInput.cpp
@@ -9,7 +9,12 @@ ModuleAnalogInput::ModuleAnalogInput(int input_pin)
 
 uint32_t ModuleAnalogInput::read()
 {
- 
+    // Force the ADC to settle down and concentrate, otherwise the
+    // previous analogRead(..) commands on other pins will cause
+    // 'crosstalk' between the input values
+    analogRead(this->pin); 
+    analogRead(this->pin);
+
     // Drop the lowest bit to reduce noise, which is present on all inputs to a 
     // certain extent.  This causes our maximum value to be 4094
     this->value = (analogRead(this->pin) >> 1) << 1;
@@ -24,26 +29,6 @@ uint32_t ModuleAnalogInput::read()
     // the this->value, which is unsigned and will overflow.
 
     this->value = constrain(map(this->value,10,4095,0,4095),0,4094);
-
-    /*
-    if(this->pin == PIN_PRG) 
-    {
-        Serial.print("PRG: ");
-        Serial.print(this->value);
-    }
-
-    if(this->pin == PIN_SR) 
-    {
-        Serial.print(" SR: ");
-        Serial.print(this->value);
-    }
-
-    if(this->pin == PIN_MOD) 
-    {
-        Serial.print(" MOD: ");
-        Serial.println(this->value);
-    }
-    */
 
     return(this->value);
 }
