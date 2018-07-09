a1255bee156089190fde50492744705e65a471e6
Budi Prakosa <iyok@deadmediafm.org>
2018-07-09 22:38:32 +0700

    move to progmem

--- global/GlobalFilterTables.h
+++ global/GlobalFilterTables.h
@@ -1,6 +1,6 @@
 #ifndef FILTER_TABLES
 #define FILTER_TABLES
 
-extern const uint16_t LPF_P_TABLE[];
-extern const uint16_t LPF_T4_TABLE[];
+extern const uint16_t LPF_P_TABLE[] PROGMEM;
+extern const uint16_t LPF_T4_TABLE[] PROGMEM;
 #endif
--- global/GlobalIncrements.h
+++ global/GlobalIncrements.h
@@ -1,6 +1,6 @@
 #ifndef Increments_h
 #define Increments_h
 
-extern const uint32_t INCREMENTS[];
+extern const uint32_t INCREMENTS[] PROGMEM;
 
 #endif
