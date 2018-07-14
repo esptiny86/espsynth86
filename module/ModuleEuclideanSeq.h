35c3840ac791308e1ccad76d2796feff0dc073a7
Budi Prakosa <iyok@deadmediafm.org>
2018-07-14 09:53:52 +0700

    add enable wifi option

---
+++ module/ModuleEuclideanSeq.h
@@ -0,0 +1,11 @@
+#ifndef MODULEEUCLIDEANSEQUENCER_H
+#define MODULEEUCLIDEANSEQUENCER_H
+
+
+class ModuleEuclideanSequencer
+{
+public:
+    ModuleEuclideanSequencer();
+};
+
+#endif // MODULEEUCLIDEANSEQUENCER_H
