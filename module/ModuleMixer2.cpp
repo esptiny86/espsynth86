d8e75549d457890a2eee8a07535dcc8d0d0c8ea5
Budi Prakosa <iyok@deadmediafm.org>
2018-07-14 10:15:46 +0700

    add mixer

---
+++ module/ModuleMixer2.cpp
@@ -0,0 +1,17 @@
+#include "ModuleMixer2.h"
+#include "defines.h"
+
+ModuleMixer2::ModuleMixer2()
+{
+	this->input_1 = NULL;
+	this->input_2 = NULL;	
+}
+
+uint16_t ModuleMixer2::compute()
+{
+	// Read inputs
+	uint32_t value_1 = this->readInput(input_1);
+	uint32_t value_2 = this->readInput(input_2);
+
+	return((value_1 + value_2)>>1);
+}
