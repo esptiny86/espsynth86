8779dbcfaab3340966dbe7870aa7dd8ffe1472bd
Budi Prakosa <iyok@deadmediafm.org>
2018-07-15 09:52:13 +0700

    vca and bit crusher

--- module/ModuleBitReducer.cpp
+++ module/ModuleBitReducer.cpp
@@ -11,7 +11,7 @@ ModuleBitReducer::ModuleBitReducer()
 uint16_t ModuleBitReducer::compute()
 {
   uint16_t audio = this->readInput(audio_input);
-  uint16_t bits = this->readInput(bit_input, 0, 12);
+  uint16_t bits = this->readInput(bit_input, 0, 15);
 
   audio = audio >> bits;  // Shave off bits for the effect
   audio = audio << bits;  // Restore to a 12 bit number
