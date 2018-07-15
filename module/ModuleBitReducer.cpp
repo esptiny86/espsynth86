7ef40e3aff8a0b2f37569993327dd0799853c7d0
Budi Prakosa <iyok@deadmediafm.org>
2018-07-15 09:52:13 +0700

    vca and bit crusher

---
+++ module/ModuleBitReducer.cpp
@@ -0,0 +1,20 @@
+#include "Arduino.h"
+#include "ModuleBitReducer.h"
+#include "defines.h"
+
+ModuleBitReducer::ModuleBitReducer()
+{
+  audio_input = NULL;
+  bit_input = NULL;  
+}
+
+uint16_t ModuleBitReducer::compute()
+{
+  uint16_t audio = this->readInput(audio_input);
+  uint16_t bits = this->readInput(bit_input, 0, 15);
+
+  audio = audio >> bits;  // Shave off bits for the effect
+  audio = audio << bits;  // Restore to a 12 bit number
+
+  return(audio);
+}
