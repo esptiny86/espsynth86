c2b96b7ae184659dc6b0e70d905b345ce25410fb
Budi Prakosa <iyok@deadmediafm.org>
2018-07-16 14:34:12 +0700

    sample player test

--- module/ModuleSamplePlayer.cpp
+++ module/ModuleSamplePlayer.cpp
@@ -1,157 +1,35 @@
-#include "Arduino.h"
 #include "ModuleSamplePlayer.h"
-#include "Defines.h"
-#include "GlobalSamples.h"
-#include "GlobalIncrements.h"
+#include "defines.h"
 
 ModuleSamplePlayer::ModuleSamplePlayer()
 {
-  t = 0;  // Sample index
-  w = 0;  // Final output
-  fixed_point_20_12_index = 0;
-  triggered = false; 
 
-  // Initialize all inputs
-  this->sample_selection_input = NULL;  
-  this->trigger_input = NULL;  
-  this->sample_rate_input = NULL;   
+  this->counter = 0;  
+  this->clocked = false;
+
+  this->trigger_input = NULL;
+  this->sample_select = NULL;
+
 }
 
 uint16_t ModuleSamplePlayer::compute()
 {
 
-  // Read inputs
-  uint16_t sample_selection = this->readInput(sample_selection_input, 0, 8);
-  uint16_t trigger = this->readInput(trigger_input);
-  uint16_t frequency = this->readInput(sample_rate_input) << 2;
-  
-  if((trigger >= MID_CV) && !triggered) 
-  {
-    triggered = true;
-    playing = true;
-    fixed_point_20_12_index = 0;
-  }
-  
-  if((trigger < MID_CV) && triggered) 
-  {
-    triggered = false;
-  }
-  
-  if(playing)
-  {
-    // shift off th 12 bits used for fractional numbers, which leaves us with a 20 bit number
-    t = fixed_point_20_12_index >> 12; 
+    uint16_t clock = this->readInput(trigger_input);
+    uint16_t samp_sel = constrain(this->readInput(sample_select,0,10),0,10) ;
 
-    switch(sample_selection)
+    if (clocked == false && clock >= MAX_CV)
     {
-
-      // ========= Drum kit #0 =============
-
-      case 0: // Kick drum
-      
-        if(t < SAMPLE_KICK_LENGTH) {
-          w = SAMPLE_KICK[t];
-        }
-        else {
-          stop_playback();
-        }
-        break;      
-  
-      case 1: // Snare drum
-
-        if(t < SAMPLE_SNARE_LENGTH) {
-          w = SAMPLE_SNARE[t];
-        }
-        else {
-          stop_playback();
-        }
-        break;
-        
-      case 2: // Hihat
-
-        if(t < SAMPLE_HIHAT_LENGTH) {
-          w = SAMPLE_HIHAT[t];
-        }
-        else {
-          stop_playback();
-        }
-        break;
-        
-      // ========= Drum kit #1 =============
-
-      case 3: // Kick drum 2
-      
-        if(t < SAMPLE_KICK2_LENGTH) {
-          w = SAMPLE_KICK2[t];
-        }
-        else {
-          stop_playback();
-        }
-        break;      
-  
-      case 4: // Snare drum 2
-
-        if(t < SAMPLE_SNARE2_LENGTH) {
-          w = SAMPLE_SNARE2[t];
-        }
-        else {
-          stop_playback();
-        }
-        break;
-        
-      case 5: // Hihat 2
-
-        if(t < SAMPLE_HIHAT2_LENGTH) {
-          w = SAMPLE_HIHAT2[t];
-        }
-        else {
-          stop_playback();
-        }
-        break;
-  
-      // ========= Drum kit #2 =============
-
-      case 6: // Kick drum 3
-      
-        if(t < SAMPLE_KICK3_LENGTH) {
-          w = SAMPLE_KICK3[t];
-        }
-        else {
-          stop_playback();
-        }
-        break;      
-  
-      case 7: // Snare drum 3
-
-        if(t < SAMPLE_SNARE3_LENGTH) {
-          w = SAMPLE_SNARE3[t];
-        }
-        else {
-          stop_playback();
-        }
-        break;
-        
-      case 8: // Hihat 3
-
-        if(t < SAMPLE_HIHAT3_LENGTH) {
-          w = SAMPLE_HIHAT3[t];
-        }
-        else {
-          stop_playback();
-        }
-        break;
-
+        clocked = true;
+        this->counter = 0;
     }
 
-    fixed_point_20_12_index += frequency;
-  }
+    if (clocked == true && clock < MAX_CV)
+        clocked = false;
 
-  return(w);
+    if (this->counter<this->sample[samp_sel].length)
+        return (pgm_read_word_near(this->sample[samp_sel].sample_loc + this->counter++)^32768);
+    else
+        return(0x8000);
+
 }
-
-void ModuleSamplePlayer::stop_playback()
-{
-  t = 0;
-  playing = false;
-}
-
--- module/ModuleSamplePlayer.h
+++ module/ModuleSamplePlayer.h
@@ -1,102 +1,45 @@
 /*
- *  +------------------------+
- *  | ModuleSamplePLayer     |
- *  |------------------------|
- *  > sample_selection_input |
- *  > trigger_input          |
- *  > sample_rate_input      |
- *  |                 output >
- *  +------------------------+
+ *
+ *      Module Sample Player
+ *
  *
  */
-// =============================================================================
-// 
-// ModuleSamplePLayer is an audio sample playback module.  All of the samples
-// available are currently drum sounds.  There is no external memory for the
-// equation composer, so it can hold very few samples.
-//
-// Samples are defined in GlobalSamples.cpp.
-//
-// Example usage:
-//
-//  ModuleDrumSequencer *drum_sequencer = new ModuleDrumSequencer();
-//  ModuleSamplePlayer *kick = new ModuleSamplePlayer();
-//  ModuleSamplePlayer *snare = new ModuleSamplePlayer();
-//  ModuleSamplePlayer *hihat = new ModuleSamplePlayer();
-//  ModuleKitSelect *kit_select = new ModuleKitSelect();
-//  ModuleMixer3 *mixer = new ModuleMixer3();
-//  ModuleWaveFolder *wave_folder = new ModuleWaveFolder();
-//  ModuleExtClock *ext_clock = new ModuleExtClock(120, EIGHTH_NOTE_CLOCK_DIVISION);
-//
-//  // Use the external clock when available, otherwise
-//  // default to the internal 120BPM clock
-//  ext_clock->clock_input = inputs->gate;
-//
-//  drum_sequencer->clock_input = ext_clock;
-//
-//  // Use the 3 parameters to select the patterns 
-//  // used for the drums.
-//  drum_sequencer->kick_pattern_input = inputs->param1;
-//  drum_sequencer->snare_pattern_input = inputs->param2;
-//  drum_sequencer->hihat_pattern_input = inputs->param3;
-//
-//  // Use the mode input to select the drum kit
-//  kit_select->kit_selection_input = inputs->mod;
-//
-//  // Set the kick, snare, and hihat inputs
-//  kick->trigger_input = drum_sequencer->kick_output;
-//  kick->sample_rate_input = inputs->sr;
-//  kick->sample_selection_input = kit_select->kick_output;
-//
-//  snare->trigger_input = drum_sequencer->snare_output;
-//  snare->sample_rate_input = inputs->sr;
-//  snare->sample_selection_input = kit_select->snare_output;
-//
-//  hihat->trigger_input = drum_sequencer->hihat_output;;
-//  hihat->sample_rate_input = inputs->sr;
-//  hihat->sample_selection_input = kit_select->hihat_output;
-//
-//  // Mix everything together
-//  mixer->input_1 = kick;
-//  mixer->input_2 = snare;
-//  mixer->input_3 = hihat;
-//
-//  // Apply some wave folding, just to make things exciting
-//  wave_folder->audio_input = mixer;
-//  wave_folder->lower_clipping_level_input = new ModuleConstant(1492);
-//  wave_folder->upper_clipping_level_input = new ModuleConstant(2604);
-//
-//  this->last_module = wave_folder;
-//
-
+ 
 #ifndef ModuleSamplePlayer_h
 #define ModuleSamplePlayer_h
 
-#include "Arduino.h"
 #include "Module.h"
+#include "drum_sampler.h"
+
+struct sample_data {
+    const uint16_t * sample_loc;
+    unsigned long length;
+};
 
 class ModuleSamplePlayer : public Module
 {
-  
   public:
     ModuleSamplePlayer();
-    uint16_t compute();
-
-    // Inputs
-    Module *sample_selection_input;
     Module *trigger_input;
-    Module *sample_rate_input;    
-    
+    Module *sample_select;
+    uint16_t compute();
   private:
-    boolean triggered;
-    boolean playing;
-    
-    uint32_t t;  // Accumulator used in equations 
-    uint32_t w;  // The final output
-    
-    uint32_t fixed_point_20_12_index;
-
-    void stop_playback();
+    bool clocked;
+    uint32_t counter;
+    sample_data sample[11] =
+    {
+        {BD16, BD16LEN},
+        {CP16, CP16LEN},
+        {CR16, CR16LEN},
+        {HH16, HH16LEN},
+        {HT16, HT16LEN},
+        {LT16, LT16LEN},
+        {MT16, MT16LEN},
+        {OH16, OH16LEN},
+        {RD16, RD16LEN},
+        {RS16, RS16LEN},
+        {SD16, SD16LEN}
+    };
 };
 
 #endif
