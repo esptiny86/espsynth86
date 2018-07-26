860539e5e443e5a6d212f7af184133a5542631ce
Budi Prakosa <iyok@deadmediafm.org>
2018-07-26 15:34:53 +0700

    euclid enable

--- module/ModuleEuclideanSeq.cpp
+++ module/ModuleEuclideanSeq.cpp
@@ -5,129 +5,122 @@
 ModuleEuclideanSeq::ModuleEuclideanSeq()
 {
 
-//    curstep = 0;
-//    pattern = 0;
-//    stepsize = 1;
-//    beatnum = 0;
-//    offsetnum = 0;
-//    counter = 0;
-//    clocked = false;
-//    rollback = false;
+    curstep = 0;
+    pattern = 0;
+    stepsize = 1;
+    beatnum = 0;
+    offsetnum = 0;
+    counter = 0;
+    clocked = false;
+    rollback = false;
 
-//    this->beat_input = NULL;
-//    this->clock_input = NULL;
-//    this->offset_input = NULL;
-//    this->step_input = NULL;
+    this->beat_input = NULL;
+    this->clock_input = NULL;
+    this->offset_input = NULL;
+    this->step_input = NULL;
 
-//    this->eu_beat.prevValue = 0;
-//    this->eu_step.prevValue = 0;
-//    this->eu_offset.prevValue = 0;
+    this->eu_beat.prevValue = 0;
+    this->eu_step.prevValue = 0;
+    this->eu_offset.prevValue = 0;
 
-//    this->setPattern(16,0);
+    this->setPattern(16,0);
 
 }
 
 uint16_t ModuleEuclideanSeq::compute()
 {
-//    uint32_t clock = this->readInput(clock_input);
+    uint32_t clock = this->readInput(clock_input);
 
-//    this->eu_beat.value = this->readInput(beat_input, 0,16);
-//    this->eu_step.value = this->readInput(step_input, 0,16);
-//    this->eu_offset.value = this->readInput(offset_input, 0,16);
+    this->eu_beat.value = this->readInput(beat_input, 0,16);
+    this->eu_step.value = this->readInput(step_input, 0,16);
+    this->eu_offset.value = this->readInput(offset_input, 0,16);
 
-//    if(this->eu_beat.value != this->eu_beat.prevValue) {
-//        this->setBeat(this->eu_beat.value);
-//        this->eu_beat.prevValue = this->eu_beat.value;
-//    }
+    if(this->eu_beat.value != this->eu_beat.prevValue) {
+        this->setBeat(this->eu_beat.value);
+        this->eu_beat.prevValue = this->eu_beat.value;
+    }
 
-//    if(this->eu_step.value != this->eu_step.prevValue) {
-//        this->setStep(this->eu_step.value);
-//        this->eu_step.prevValue = this->eu_step.value;
-//    }
+    if(this->eu_step.value != this->eu_step.prevValue) {
+        this->setStep(this->eu_step.value);
+        this->eu_step.prevValue = this->eu_step.value;
+    }
 
-//    if(this->eu_offset.value != this->eu_offset.prevValue) {
-//        this->setOffset(this->eu_offset.value);
-//        this->eu_offset.prevValue = this->eu_offset.value;
-//    }
+    if(this->eu_offset.value != this->eu_offset.prevValue) {
+        this->setOffset(this->eu_offset.value);
+        this->eu_offset.prevValue = this->eu_offset.value;
+    }
 
-//    if((clock < MID_CV) && clocked)
-//    {
-//        clocked = false;
-//    }
+    if((clock < MID_CV) && clocked)
+    {
+        clocked = false;
+    }
 
-//    if((clock >= MID_CV) && !clocked)
-//    {
-//      clocked = true;
-//      counter = counter + 1;
-//    }
+    if((clock >= MID_CV) && !clocked)
+    {
+      clocked = true;
+      counter = counter + 1;
+    }
 
-//    return getPatternTick(counter) * MAX_CV;
-    return 0;
+    return getPatternTick(counter) * MAX_CV;
 
 }
 
 void ModuleEuclideanSeq::setPattern(uint8_t step, uint8_t beat, uint8_t offset)
 {
-//    stepsize = step;
-//    beatnum = beat;
-//    offsetnum = offset;
-//    pattern = euclidean_arr(step, beat, offset);
+    stepsize = step;
+    beatnum = beat;
+    offsetnum = offset;
+    pattern = euclidean_arr(step, beat, offset);
 }
 
 void ModuleEuclideanSeq::setPattern(uint8_t step, uint8_t beat)
 {
-//    setPattern(step,beat,0);
+    setPattern(step,beat,0);
 }
 
 void ModuleEuclideanSeq::setBeat(uint8_t beat)
 {
-//    if (beat>stepsize) stepsize = beat;
-//    setPattern(stepsize, beat, offsetnum);
+    if (beat>stepsize) stepsize = beat;
+    setPattern(stepsize, beat, offsetnum);
 }
 
 void ModuleEuclideanSeq::setStep(uint8_t step)
 {
-//    if (beatnum>step) beatnum = step;
-//    setPattern(step, beatnum, offsetnum);
+    if (beatnum>step) beatnum = step;
+    setPattern(step, beatnum, offsetnum);
 }
 
 void ModuleEuclideanSeq::setOffset(uint8_t offset)
 {
-//    setPattern(stepsize, beatnum, offset);
+    setPattern(stepsize, beatnum, offset);
 }
 
 void ModuleEuclideanSeq::setRollback()
 {
-//    rollback = true;
+    rollback = true;
 }
 
 uint32_t ModuleEuclideanSeq::nextPattern()
 {
-//    uint32_t pat = getPattern(curstep);
-//    curstep++;
-//    if (curstep> (stepsize-1) ) curstep = 0;
-//    return pat;
-    return 0;
-
+    uint32_t pat = getPattern(curstep);
+    curstep++;
+    if (curstep> (stepsize-1) ) curstep = 0;
+    return pat;
 }
 
 uint32_t ModuleEuclideanSeq::getPattern(uint8_t step)
 {
-//    return (pattern & (1<<step));
-    return 0;
-
+    return (pattern & (1<<step));
 }
 
 uint32_t ModuleEuclideanSeq::getPatternTick(uint32_t step)
 {
-//    if (rollback){
-//       curstep = (curstep+1)%stepsize;
-//       rollback = false;
-//    }else{
-//       curstep = step%stepsize;
-//    }
-//    return (pattern & (1<<curstep));
-    return 0;
-
+    if (rollback){
+       curstep = (curstep+1)%stepsize;
+       rollback = false;
+    }else{
+       curstep = step%stepsize;
+    }
+    return (pattern & (1<<curstep));
 }
 
