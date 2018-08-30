3de24a12ddaf1615651b68da0dd1db5b28aab196
Budi Prakosa <iyok@deadmediafm.org>
2018-08-30 14:45:28 +0700

    clean build tools

--- build_lib.sh
+++ build_lib.sh
@@ -1,4 +1,5 @@
 COMPILE_PATH=/tmp/mkESP/synthmodule86_d1_mini
+AR_PATH=/Users/xcorex/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2/bin/xtensa-lx106-elf-ar
 
 mkdir -p ./src/esp8266
 
@@ -12,13 +13,11 @@ make
 
 cd lib
 
-TEST=`ls *.cpp | while read f; do echo ${COMPILE_PATH}/${f}.o; done;`
+LIB_FILES=`ls *.cpp | while read f; do echo ${COMPILE_PATH}/${f}.o; done;`
 
 cd ..
 
-# echo ${TEST}
-
-/Users/xcorex/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2/bin/xtensa-lx106-elf-ar rvs ./src/esp8266/libespsynth86.a ${TEST}
+${AR_PATH} rvs ./src/esp8266/libespsynth86.a ${LIB_FILES}
 
 
 # /Users/xcorex/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2/bin/xtensa-lx106-elf-ar rvs espsynth86.a /tmp/mkESP/synthmodule86_d1_mini/Global*.o
