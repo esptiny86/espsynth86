c39bbbe217ed32083de3719cdb7743246bf4b614
Budi Prakosa <iyok@deadmediafm.org>
2019-04-01 06:43:56 +0700

    add bundle reader

--- src/OSCBundleReader.cpp
+++ src/OSCBundleReader.cpp
@@ -1,13 +1,53 @@
 #include "OSCBundleReader.h"
 
 
-OSCBundleReader::OSCBundleReader(WiFiUDP *UdpOsc)
+OSCBundleReader::OSCBundleReader(WiFiUDP *UdpOsc, OSC_TYPE osc_type)
 {
     this->Udp = UdpOsc;
+    this->oscType = osc_type;
 }
 
 
 void OSCBundleReader::readToInputManager(Input_Manager *inputManager)
+{
+    if (this->oscType == OSC_MESSAGE){
+        this->readOSCMessage(inputManager);
+    }else{
+        this->readOSCBundle(inputManager);
+    }
+}
+
+void OSCBundleReader::readOSCBundle(Input_Manager *inputManager)
+{
+    OSCMessage osc_msg_rcv;
+    OSCBundle osc_msg_rcv_bnd;
+
+    int size = Udp->parsePacket();
+    if (size > 0) {
+      while (size--) {
+        osc_msg_rcv_bnd.fill(Udp->read());
+      }
+      if (!osc_msg_rcv_bnd.hasError()) {
+        for(int i=0; i<inputManager->osc.size(); ++i) {
+
+            OSCMessage msg = osc_msg_rcv_bnd.getOSCMessage(0);
+
+            if(msg.fullMatch(inputManager->osc[i]->address, 0))
+            {
+              if (this->oscType == OSC_VCV_LEMUR)
+              {
+                  inputManager->osc[i]->setValue(msg.getInt(0));
+              }else{
+                  inputManager->osc[i]->setValue(msg.getFloat(0)*1023);
+              }
+
+            }
+        }
+      }
+}
+}
+
+void OSCBundleReader::readOSCMessage(Input_Manager *inputManager)
 {
     OSCMessage osc_msg_rcv;
     int size = Udp->parsePacket();
@@ -19,14 +59,10 @@ void OSCBundleReader::readToInputManager(Input_Manager *inputManager)
         for(int i=0; i<inputManager->osc.size(); ++i) {
             if(osc_msg_rcv.fullMatch(inputManager->osc[i]->address, 0))
             {
-//              #ifdef DEBUG_SERIAL
-              Serial.print(inputManager->osc[i]->address);
-              Serial.print(" VAL: ");
-              Serial.println(osc_msg_rcv.getInt(0));
-//              #endif
               inputManager->osc[i]->setValue(osc_msg_rcv.getInt(0));
             }
         }
       }
     }
 }
+
--- src/OSCBundleReader.h
+++ src/OSCBundleReader.h
@@ -7,13 +7,24 @@
 #include <OSCData.h>
 #include <ModuleExternalInput.h>
 
+enum OSC_TYPE {
+    OSC_MESSAGE,
+    OSC_BUNDLE,
+    OSC_VCV_LEMUR,
+    OSC_TOUCHOSC
+};
+
 class OSCBundleReader
 {
 public:
-    OSCBundleReader(WiFiUDP* UdpOsc);
+    OSCBundleReader(WiFiUDP* UdpOsc, OSC_TYPE osc_type);
     void readToInputManager(Input_Manager * inputManager);
+    void readOSCBundle(Input_Manager * inputManager);
+    void readOSCMessage(Input_Manager * inputManager);
+
 private:
     WiFiUDP* Udp;
+    OSC_TYPE oscType;
 };
 
 #endif // OSCBUNDLEREADER_H
